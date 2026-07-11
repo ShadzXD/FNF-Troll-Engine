5bf3012233d04178d9399215d4fa7f8596cf515a
riconuts <68708956+riconuts@users.noreply.github.com>
2026-07-10 23:49:18 -0400

    Use hxvlc 2.3.0

--- setup/unix-haxelibs.sh
+++ setup/unix-haxelibs.sh
@@ -7,7 +7,7 @@ haxelib --always --quiet install flixel-addons 3.2.3
 haxelib --always --quiet git hxcpp https://github.com/moxie-coder/hxcpp-funkin troll-engine
 haxelib --always --quiet git hscript https://github.com/troll-slaiyers/t-hscript
 haxelib --always --quiet install no-spoon 0.2.0
-haxelib --always --quiet  --skip-dependencies install hxvlc 2.2.6
+haxelib --always --quiet  --skip-dependencies install hxvlc 2.3.0
 haxelib --always --quiet install hxdiscord_rpc 1.3.0
 haxelib --always --quiet install moonchart 0.5.1
 haxelib --always --quiet install flixel-animate 1.5.0
